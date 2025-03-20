d1d4d8d1e2cf30085973375103863a571ff25ecd
Algama17 <albertogarciamallada@gmail.com>
2025-03-20 09:48:30 +0100

    first commit

---
+++ src/main/java/picnic_f5/picnic_f5/PicnicF5Application.java
@@ -0,0 +1,13 @@
+package picnic_f5.picnic_f5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicnicF5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicnicF5Application.class, args);
+	}
+
+}
